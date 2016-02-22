4449fcabc123e4d3c46209f521da6ac7ebdb8bcb
johnny-alibuyog <johnny.alibuyog@gmail.com>
2016-02-22 00:07:49 +0000

    changed action text to icons. added search functionality

--- Tinda/Tinda.Web/src/products/product-list.ts
+++ Tinda/Tinda.Web/src/products/product-list.ts
@@ -1,27 +1,28 @@
 export class ProductList {
+  mockData = [];
   productInventories = [];
-  counter: number = 0;
+  filterText: string = '';
 
   constructor() {
-
-    this.productInventories = [
-      /*
-            { id: 1, productCode: "NWTB-1", productName: "Product Name 1", category: "Category 1", supplier: "Supplier A", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-            { id: 2, productCode: "NWTB-2", productName: "Product Name 1", category: "Category 2", supplier: "Supplier B", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-            { id: 3, productCode: "NWTB-3", productName: "Product Name 1", category: "Category 3", supplier: "Supplier C", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-            { id: 4, productCode: "NWTB-4", productName: "Product Name 1", category: "Category 4", supplier: "Supplier D", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-            { id: 5, productCode: "NWTB-5", productName: "Product Name 1", category: "Category 5", supplier: "Supplier E", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-      */
+    this.mockData = [
       { id: this.guid(), productCode: "NWTB-1", productName: "Product Name 1", category: "Category 1", supplier: "Supplier A", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-      { id: this.guid(), productCode: "NWTB-2", productName: "Product Name 1", category: "Category 2", supplier: "Supplier B", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-      { id: this.guid(), productCode: "NWTB-3", productName: "Product Name 1", category: "Category 3", supplier: "Supplier C", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-      { id: this.guid(), productCode: "NWTB-4", productName: "Product Name 1", category: "Category 4", supplier: "Supplier D", standardCost: "₱0.00", listPrice: "₱10,000.00" },
-      { id: this.guid(), productCode: "NWTB-5", productName: "Product Name 1", category: "Category 5", supplier: "Supplier E", standardCost: "₱0.00", listPrice: "₱10,000.00" },
+      { id: this.guid(), productCode: "NWTB-2", productName: "Product Name 2", category: "Category 2", supplier: "Supplier B", standardCost: "₱0.00", listPrice: "₱10,000.00" },
+      { id: this.guid(), productCode: "NWTB-3", productName: "Product Name 3", category: "Category 3", supplier: "Supplier C", standardCost: "₱0.00", listPrice: "₱10,000.00" },
+      { id: this.guid(), productCode: "NWTB-4", productName: "Product Name 4", category: "Category 4", supplier: "Supplier D", standardCost: "₱0.00", listPrice: "₱10,000.00" },
+      { id: this.guid(), productCode: "NWTB-5", productName: "Product Name 5", category: "Category 5", supplier: "Supplier E", standardCost: "₱0.00", listPrice: "₱10,000.00" },
     ];
+
+    this.productInventories = this.mockData;
+  }
+
+  filter() {
+    this.productInventories = this.mockData.filter(x => {
+      //return x.productName.lastIndexOf(this.filterText, 0) === 0;
+      return x.productName.search(new RegExp(this.filterText, "i")) != -1;
+    });
   }
 
   guid() {
-    /*return this.counter++;*/
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
       return v.toString(16);
