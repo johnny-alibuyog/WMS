431b51fece04df91a00c980662985ab83ab2b22a
johnny-alibuyog <johnny.alibuyog@gmail.com>
2016-05-27 06:55:57 +0000

    handler base

---
+++ AmpedBiz/AmpedBiz.Service/RequestHandlerBase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using NHibernate;
+
+namespace AmpedBiz.Service
+{
+    public abstract class RequestHandlerBase<TRequest, TResponse> : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        protected readonly ISessionFactory _sessionFactory;
+
+        public abstract TResponse Handle(TRequest message);
+
+        public RequestHandlerBase(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+    }
+}
