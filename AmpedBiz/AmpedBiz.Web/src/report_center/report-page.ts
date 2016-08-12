c60e1282bd3be1329b0683837598d525a87ee2eb
jsahagun <jsahagun>
2016-08-12 08:10:03 +0000

    cleanup

--- AmpedBiz/AmpedBiz.Web/src/report_center/report-page.ts
+++ AmpedBiz/AmpedBiz.Web/src/report_center/report-page.ts
@@ -8,7 +8,6 @@ export class ReportPage {
   private _api: ServiceApi;
   private _router: Router;
 
-  public header: string = ' Reports';
   public reportPath: string = '';
 
   constructor(api: ServiceApi, router: Router) {
@@ -17,19 +16,6 @@ export class ReportPage {
   }
 
   activate(params: any, routeConfig: RouteConfig, $navigationInstruction: NavigationInstruction): any {
-    /*
-    let requests: [Promise<Lookup<string>[]>, Promise<Lookup<OrderStatus>[]>] = [
-      this._api.customers.getLookups(),
-      this._api.orders.getStatusLookup()
-    ];
-
-    Promise.all(requests).then((responses: [Lookup<string>[], Lookup<OrderStatus>[]]) => {
-      this.customers = responses[0];
-      this.statuses = responses[1];
-      this.filter["status"] = routeConfig.settings.status;
-      this.header = routeConfig.title;
-      this.getPage();
-    });*/
     this.reportPath = routeConfig.settings.reportSource;
   }
 
