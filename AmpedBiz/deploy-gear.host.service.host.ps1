75001b7685b6fd47a0704ff6989c23b970b1f469
johnny-alibuyog <johnny.alibuyog@gmail.com>
2017-02-25 08:05:39 +0000

    added ftp in publish profile, MSDeploy is currently blocked by ZScaler

--- AmpedBiz/deploy-gear.host.service.host.ps1
+++ AmpedBiz/deploy-gear.host.service.host.ps1
@@ -1,6 +1,6 @@
 $env:username="`$ampbiz-api"
 $env:password="i1JRyJXk4Zvl1jxnp0qwDMlkmD6Gw2Amce00HyHew1qQL7HFQpBPcbhqmwQe"
-$env:publish_profile="ampbiz-api.staging.gear.host.pubxml"
+$env:publish_profile="staging-gear.host.pubxml"
 $env:database_config="database.staging.gear.host.config.json"
 
 .\build.ps1 -target Deploy
