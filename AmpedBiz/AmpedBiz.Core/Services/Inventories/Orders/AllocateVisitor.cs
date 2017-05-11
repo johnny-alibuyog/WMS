f23c41ea2bf923094781b1086ea6c01818f96591
johnny-alibuyog <johnny.alibuyog@gmail.com>
2017-05-11 13:07:28 +0000

    order quantity validation

--- AmpedBiz/AmpedBiz.Core/Services/Inventories/Orders/AllocateVisitor.cs
+++ AmpedBiz/AmpedBiz.Core/Services/Inventories/Orders/AllocateVisitor.cs
@@ -1,4 +1,5 @@
-﻿using AmpedBiz.Core.Entities;
+﻿using AmpedBiz.Common.Exceptions;
+using AmpedBiz.Core.Entities;
 
 namespace AmpedBiz.Core.Services.Inventories.Orders
 {
@@ -8,6 +9,15 @@ namespace AmpedBiz.Core.Services.Inventories.Orders
 
         public override void Visit(Inventory target)
         {
+            if (target.Available < this.Quantity)
+            {
+                throw new BusinessException(
+                    $"Unable to invoice {target.Product.Name}." +
+                    $"Order quantity is {Quantity.Value} {Quantity.Unit.Id} " + 
+                    $"while only {target.Available.Value} {target.Available.Unit.Id} are available."
+                );
+            }
+            
             target.Allocated += Quantity;
             target.Accept(new InventoryRecomputeVisitor());
         }
--- AmpedBiz/AmpedBiz.Web/src/purchases/purchase-order-create.ts
+++ AmpedBiz/AmpedBiz.Web/src/purchases/purchase-order-create.ts
@@ -8,6 +8,7 @@ import { PurchaseOrder, PurchaseOrderStatus, PurchaseOrderAggregate, PurchaseOrd
 import { ServiceApi } from '../services/service-api';
 import { NotificationService } from '../common/controls/notification-service';
 import { pricing } from '../common/models/pricing';
+import { formatDate } from '../services/formaters';
 import { VoucherReport } from './voucher-report';
 
 @autoinject
@@ -167,6 +168,86 @@ export class PurchaseOrderCreate {
   public showVoucher(): void {
     this._api.purchaseOrders.getVoucher(this.purchaseOrder.id)
       .then(data => this._voucherReport.show(data))
+  }
+
+  // NOTE: I HATE THIS METHOD!!!, needed to be refactored when demo is done.
+  // REFERENCE: https://github.com/kinlane/csv-converter/blob/gh-pages/json-to-csv/index.html 
+  public downloadVoucherCsv(): void {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = this.purchaseOrder.items.map(x => <any>{
+        Product: x.product.name,
+        Quantity: x.quantityValue,
+        Cost: x.unitCostAmount,
+        Total: x.totalCostAmount
+      });
+
+    var $csv = 'sep=,' + '\r\n\n';
+    var showLabel = true;
+    var reportTitle = "Voucher";
+
+    //This condition will generate the Label/Header
+    if (showLabel) {
+      var row = "";
+
+      //This loop will extract the label from 1st index of on array
+      for (var index in arrData[0]) {
+
+        //Now convert each value to string and comma-seprated
+        row += index + ',';
+      }
+
+      row = row.slice(0, -1);
+
+      //append Label row with line break
+      $csv += row + '\r\n';
+    }
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+      var row = "";
+
+      //2nd loop will extract each column and convert it in string comma-seprated
+      for (var index in arrData[i]) {
+        row += '"' + arrData[i][index] + '",';
+      }
+
+      row.slice(0, row.length - 1);
+
+      //add a line break after each row
+      $csv += row + '\r\n';
+    }
+
+    if ($csv == '') {
+      alert("Invalid data");
+      return;
+    }
+
+    //Generate a file name
+    var fileName = formatDate(new Date);
+
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += reportTitle.replace(/ /g, "_");
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + encodeURI($csv);
+
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    //link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
   public reject(): void {
