0582733f1d68f522926bc8ac6ec0b91638e7b3ff
johnny-alibuyog <johnny.alibuyog@gmail.com>
2018-10-16 02:50:06 +0000

    - fix build error

---
+++ AmpedBiz/AmpedBiz.Core/Services/Orders/OrderItemReturnVisitor.cs
@@ -0,0 +1,26 @@
+﻿using AmpedBiz.Common.Extentions;
+using AmpedBiz.Core.Entities;
+
+namespace AmpedBiz.Core.Services.Orders
+{
+	public class OrderItemReturnVisitor : IVisitor<OrderItem>
+	{
+		public OrderReturn Return { get; private set; }
+
+		public OrderItemReturnVisitor(OrderReturn @return)
+		{
+			this.Return = @return;
+		}
+
+		public void Visit(OrderItem target)
+		{
+			target.Ensure(
+				that: (item) => item.Quantity >= this.Return.Quantity, 
+				message: $"You can not return {this.Return.Quantity.ToStringWithSymbol()} because only {target.Quantity.ToStringWithSymbol()} is in order."
+			);
+
+			target.Quantity -= this.Return.Quantity;
+			target.Compute();
+		}
+	}
+}
