61ad15966b4787a30fc05cebaa4dd7f73f39f7d6
johnny-alibuyog <johnny.alibuyog@gmail.com>
2022-09-27 19:46:51 -0400

    Cleanup

--- AmpedBiz/deploy-gear.host.service.host.ps1
+++
@@ -1,6 +0,0 @@
-$env:username="`$ampbiz-api"
-$env:password="F1d1mtdk0YkA2wKSTMsQS29hz58SSj65S4Ln7MJS6m9FHnruxFyL4zeDYl4p"
-$env:publish_profile="staging-gear.host.pubxml"
-$env:database_config="database.staging.gear.host.config.json"
-
-.\build.ps1 -target Deploy
--- AmpedBiz/deploy-gear.host.web.ps1
+++
@@ -1,8 +0,0 @@
-cd .\AmpedBiz.Web\
-
-$env:host="ftp.gear.host"
-$env:folder="/site/wwwroot"
-$env:username="ampbiz"
-$env:password="65to4ldYZFmaubEeBhTn97aow7aXbWo07EAdWHYGHTdgMCoTz4Zrl2wKBFYv"
-
-gulp deploy --host $env:host --folder $env:folder --username $env:username --password $env:password
